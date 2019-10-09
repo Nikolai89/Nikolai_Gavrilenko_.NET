295bb510d592d2e9b8d83cbeb208ddc739ec1fb2
Module1_1 <nikolaigavrilenko89@gmail.com>
2019-10-09 17:19:45 +0300

    New project was created. Task 1 was completed!

---
+++ Training/TestRepo/Module1_1/Module1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine($"a={b} b={a}");
+        }
+    }
+}
